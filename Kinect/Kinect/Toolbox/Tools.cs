828a3b5c93513bfcacf3de4199c19c47641defba
Diogo Teixeira <diogo.andre.teixeira@gmail.com>
2013-09-12 23:00:50 +0100

    Initial commit.

---
+++ Kinect/Kinect/Toolbox/Tools.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Kinect;
+
+namespace Kinect.Toolbox
+{
+    /// <summary>
+    /// This class contains some static methods to some basic but important utilities.
+    /// </summary>
+    public class Tools
+    {
+        /// <summary>
+        /// This utility method converts a skeleton point to a 2D space
+        /// vector.
+        /// </summary>
+        /// <param name="sensor">Kinect sensor</param>
+        /// <param name="position">Position of the joint</param>
+        /// <returns>A 2D vector.</returns>
+        public static Vector2 ConvertSkeletonPointToScreen(KinectSensor sensor, SkeletonPoint position)
+        {
+            float width = 0;
+            float height = 0;
+            float x = 0;
+            float y = 0;
+
+            // In case the ColorStream is enabled it can be used to
+            // compute the world to screen 2D vector.
+            if (sensor.ColorStream.IsEnabled)
+            {
+                // Maps the skeleton point to a corresponding color point.
+                var colorPoint = sensor.CoordinateMapper.MapSkeletonPointToColorPoint(position, sensor.ColorStream.Format);
+                x = colorPoint.X;
+                y = colorPoint.Y;
+
+                // Obtains the real dimensions of the stream.
+                switch (sensor.ColorStream.Format)
+                {
+                    case ColorImageFormat.RawYuvResolution640x480Fps15:
+                    case ColorImageFormat.RgbResolution640x480Fps30:
+                    case ColorImageFormat.YuvResolution640x480Fps15:
+                        width = 640;
+                        height = 480;
+                        break;
+
+                    case ColorImageFormat.RgbResolution1280x960Fps12:
+                        width = 1280;
+                        height = 960;
+                        break;
+                }
+            }
+            // The DepthStream can also be used to compute the vector, and
+            // is often more precise.
+            else if (sensor.DepthStream.IsEnabled)
+            {
+                // Maps the skeleton point to a corresponding depth point.
+                var depthPoint = sensor.CoordinateMapper.MapSkeletonPointToDepthPoint(position, sensor.DepthStream.Format);
+                x = depthPoint.X;
+                y = depthPoint.Y;
+
+                // Obtains the real dimensions of the stream.
+                switch (sensor.DepthStream.Format)
+                {
+                    case DepthImageFormat.Resolution80x60Fps30:
+                        width = 80;
+                        height = 60;
+                        break;
+
+                    case DepthImageFormat.Resolution320x240Fps30:
+                        width = 320;
+                        height = 240;
+                        break;
+
+                    case DepthImageFormat.Resolution640x480Fps30:
+                        width = 640;
+                        height = 480;
+                        break;
+                }
+            }
+            // Without any of the previous streams, no useful information can be
+            // computed; vector is returned as is.
+            else
+            {
+                width = 1;
+                height = 1;
+            }
+
+            return new Vector2(x / width, y / height);
+        }
+    }
+}
---
+++ KinectShooter/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Kinect.Sensor;
+using Kinect.Toolbox;
+using Microsoft.Kinect;
+
+namespace KinectShooter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private KinectSensorController controller;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            controller = new KinectSensorController(KinectSensorType.Xbox360Sensor);
+            controller.TrackedSkeletonReady += new KinectSensorController.TrackedSkeletonReadyHandler(callback_TrackedSkeletonReady);
+            controller.StartSensor();
+        }
+
+        public void callback_TrackedSkeletonReady(Skeleton skeleton) 
+        {
+            MainCanvas.Children.Clear();
+            foreach(Joint joint in skeleton.Joints)
+            {
+                Vector2 vector2 = Tools.ConvertSkeletonPointToScreen(controller.Sensor, joint.Position);
+                float centerX = (float)((vector2.X * MainCanvas.ActualWidth / 2.0) + (MainCanvas.ActualWidth / 4.0));
+                float centerY = (float)((vector2.Y * MainCanvas.ActualHeight / 2.0) + (MainCanvas.ActualHeight / 4.0));
+                const double diameter = 8;
+
+                Ellipse ellipse = new Ellipse
+                {
+                    Width = diameter,
+                    Height = diameter,
+                    HorizontalAlignment = HorizontalAlignment.Left,
+                    VerticalAlignment = VerticalAlignment.Top,
+                    Fill = Brushes.SkyBlue
+                };
+                Canvas.SetLeft(ellipse, centerX - ellipse.Width / 2);
+                Canvas.SetTop(ellipse, centerY - ellipse.Height / 2);
+                MainCanvas.Children.Add(ellipse);
+            }
+        }
+    }
+}
